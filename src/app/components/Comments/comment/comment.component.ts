ee7502cb9267c87ed575c145c561ddf022fa0447
Alaiddin Bilginer <alaiddinbilginer@outlook.com>
2024-12-24 13:51:36 +0300

    [Feature]: Implement delete functionality for comments and their replies

--- src/app/components/Comments/comment/comment.component.ts
+++ src/app/components/Comments/comment/comment.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { Reply } from '../../../models/reply';
 import { Comment } from '../../../models/comment';
 import { CommentService } from '../../../services/models/comment.service';
 import { TimeAgo } from '../../../pipes/time-ago.pipe';
 import { ReplyComponent } from '../reply/reply.component';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import { faComments, faThumbsUp, faThumbsDown, faArrowAltCircleDown } from '@fortawesome/free-regular-svg-icons'
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReplyCommentComponent } from '../reply-comment/reply-comment.component';
 import { IdentityService } from '../../../services/auth/identity.service';
+import { NotificationService } from '../../../services/common/notification.service';
 
 @Component({
   selector: 'app-comment',
@@ -19,6 +21,7 @@ import { IdentityService } from '../../../services/auth/identity.service';
 export class CommentComponent implements OnInit {
   @Input() postId: string = '';
   @Input() comment!: Comment;
+  @Output() refreshListing = new EventEmitter<void>();
 
   replies: Reply[] = [];
   tempReplies: Reply[] = [];
@@ -27,18 +30,21 @@ export class CommentComponent implements OnInit {
   replying: boolean = false;
   page: number = 1;
   size: number = 6;
+  activeMenuId: string | null = null;
 
   faComments = faComments;
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
   faArrowAltCircleDown = faArrowAltCircleDown;
+  faEllipsisV = faEllipsisV;
 
   constructor(
     private commentService: CommentService,
     library: FaIconLibrary,
-    public identityService: IdentityService
+    public identityService: IdentityService,
+    private notificationService: NotificationService,
   ) {
-    library.addIcons(faComments, faThumbsUp, faThumbsDown, faArrowAltCircleDown);
+    library.addIcons(faComments, faThumbsUp, faThumbsDown, faArrowAltCircleDown, faEllipsisV);
   }
 
   ngOnInit(): void {
@@ -54,17 +60,21 @@ export class CommentComponent implements OnInit {
     }
   }
   
-  fetchReplies(page: number = 0): void {
+  fetchReplies(): void {
+    this.page = 1;
     this.commentService.getRepliesByParentComment(this.comment.id, (this.page - 1), this.size)
       .subscribe((response) => {
-        this.replies = [...this.replies, ...response.replies]
+        this.replies = response.replies;
       });
   }
 
   showMoreReplies() {
     this.page++;
     this.tempReplies = [];
-    this.fetchReplies(this.page);
+    this.commentService.getRepliesByParentComment(this.comment.id, (this.page - 1), this.size)
+      .subscribe((response) => {
+        this.replies = [...this.replies, ...response.replies]
+      });
   }
 
   toggleReply(): void {
@@ -75,5 +85,50 @@ export class CommentComponent implements OnInit {
     this.tempReplies.unshift(reply);
     this.comment.totalRepliesCount++;
     this.showTempReplies = true;
+  }
+
+  toggleMenu(commentId: string): void {
+    this.activeMenuId = this.activeMenuId === commentId ? null : commentId;
+  }
+
+  editComment(commentId: string): void {
+
+    this.activeMenuId = null;
+  }
+
+  deleteComment(commentId: string): void {
+    this.notificationService.confirmDialog(
+      "Emin Misiniz?", "Yorumunuzu silmek istediğinizden emin misiniz?",
+    'Evet', 'Başarılı', 'Yorumunuz silindi', () => {
+      this.commentService.deleteComment(commentId).subscribe((response) => {
+        this.refreshListing.emit();
+      });
+    });
+    this.activeMenuId = null;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    if(!target.closest('.dropdown')) {
+      this.activeMenuId = null;
+    }
+  }
+
+  refresReplieshDelete(reply: Reply) {
+    const deletedReply = this.replies.indexOf(reply);
+    if(deletedReply !== -1) {
+      this.replies.splice(deletedReply, 1);
+      this.comment.totalRepliesCount--;
+    }
+  }
+
+  refreshTempRepliesDelete(reply: Reply) {
+    const deletedReply = this.tempReplies.indexOf(reply);
+    if(deletedReply !== -1) {
+      this.tempReplies.splice(deletedReply, 1);
+      this.comment.totalRepliesCount--;
+    }
   }
 }
--- src/app/components/Comments/reply/reply.component.ts
+++ src/app/components/Comments/reply/reply.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { Reply } from '../../../models/reply';
 import { TimeAgo } from '../../../pipes/time-ago.pipe';
 import { faComments, faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReplyCommentComponent } from '../reply-comment/reply-comment.component';
 import { Comment } from '../../../models/comment';
 import { IdentityService } from '../../../services/auth/identity.service';
+import { NotificationService } from '../../../services/common/notification.service';
+import { CommentService } from '../../../services/models/comment.service';
 
 @Component({
   selector: 'app-reply',
@@ -18,18 +21,23 @@ export class ReplyComponent {
   @Input() reply!: Reply;
   @Input() postId: string = '';
   @Input() parentComment!: Comment;
+  @Output() refreshReplies = new EventEmitter<Reply>();
 
   replying: boolean = false;
   showTempReplies: boolean = true;
   tempReplies: Reply[] = [];
+  activeMenuId: string | null = null;
 
   faComments = faComments;
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
+  faEllipsisV = faEllipsisV;
   
   constructor(
     library: FaIconLibrary,
-    public identityService: IdentityService
+    public identityService: IdentityService,
+    private notificationService: NotificationService,
+    private commentService: CommentService
   ) {
     library.addIcons(faComments, faThumbsUp, faThumbsDown);
   }
@@ -42,5 +50,34 @@ export class ReplyComponent {
     this.tempReplies.unshift(reply);
     this.parentComment.totalRepliesCount++;
     this.showTempReplies = true;
+  }
+
+  toggleMenu(commentId: string): void {
+    this.activeMenuId = this.activeMenuId === commentId ? null : commentId;
+  }
+
+  editComment(commentId: string): void {
+
+    this.activeMenuId = null;
+  }
+
+  deleteComment(commentId: string): void {
+    this.notificationService.confirmDialog(
+      "Emin Misiniz?", "Yorumunuzu silmek istediğinizden emin misiniz?",
+    'Evet', 'Başarılı', 'Yorumunuz silindi', () => {
+      this.commentService.deleteComment(commentId).subscribe((response) => {
+        this.refreshReplies.emit(this.reply);
+      });
+    });
+    this.activeMenuId = null;
+  }
+  
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    if(!target.closest('.dropdown')) {
+      this.activeMenuId = null;
+    }
   }
 }
